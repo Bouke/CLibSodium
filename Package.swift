0244e3b03f41bb4e144ea290aeb6c494e5b7e9e0
Bouke Haarsma <bouke@haarsma.eu>
2019-03-31 11:35:11 +0200

    Upgrade to 4.2 SwiftPM manifest

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
     name: "CLibSodium",
     pkgConfig: "libsodium",
     providers: [
-        .Brew("libsodium"),
-        .Apt("libsodium-dev")
+        .brew(["libsodium"]),
+        .apt(["libsodium-dev"])
     ]
 )
